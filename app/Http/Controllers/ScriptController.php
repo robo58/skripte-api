779d0540024097ea32d1e3234ceed8b50db81323
robo58 <robert.sliskovic@fsre.sum.ba>
2022-06-06 12:28:51 +0200

    file on script creation

--- app/Http/Controllers/ScriptController.php
+++ app/Http/Controllers/ScriptController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\File;
 use App\Models\Script;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Orion\Concerns\DisableAuthorization;
 use Orion\Concerns\DisablePagination;
 use Orion\Http\Controllers\Controller;
+use Orion\Http\Requests\Request;
 
 class ScriptController extends Controller
 {
     use DisablePagination, DisableAuthorization;
     protected $model=Script::class;
+
+    protected function afterSave(Request $request, Model $entity)
+    {
+        $file=$request->script;
+        $filename = md5($file) . now()->timestamp;
+
+        $filename .= '.' . $file->getClientOriginalExtension();
+        $path = basename(Storage::disk('local')->putFileAs('scripts', $file, $filename));
+        File::create([
+            'name'=>$path,
+            'script_id'=>$entity->id
+        ]);
+    }
 }
