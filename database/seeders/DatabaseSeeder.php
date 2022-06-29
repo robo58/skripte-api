5aa26d7cd5a1d4bc0fd53d0fa73efbc0fc9cc87f
robo58 <robert.sliskovic@fsre.sum.ba>
2022-06-29 14:43:06 +0200

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,13 @@ class DatabaseSeeder extends Seeder
 
         Role::create(['name'=>'admin']);
         Role::create(['name'=>'user']);
+
+        $user = User::create([
+            'name'=>'admin',
+            'email'=>'admin@admin.com',
+            'password'=>bcrypt('admin'),
+        ]);
+        $user->assignRole('admin');
+        $user->assignRole('user');
     }
 }
